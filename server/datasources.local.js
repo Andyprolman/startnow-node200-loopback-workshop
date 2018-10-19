0379856cd22b3a0536f1a778125b35b1074f9760
Andrew Prolman <aprolman21@gmail.com>
2018-10-19 11:33:05 -0700

    fixes es lint errors

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,17 +1,17 @@
+'use strict';
 module.exports = {
-    "db": {
-      "name": "db",
-      "connector": "memory"
-    },
-  
-    "mongoDB": {
-      "host": "",
-      "port": 0,
-      "url": "",
-      "database": "",
-      "password": "",
-      "name": "mongoDB",
-      "user": "",
-      "connector": "mongodb"
-    }
-  }
+  'db': {
+    'name': 'db',
+    'connector': 'memory',
+  },
+  'mongoDB;': {
+    'host': '',
+    'port': 0,
+    'url': '',
+    'database': '',
+    'password': '',
+    'name': 'mongoDB',
+    'user': '',
+    'connector': 'mongodb',
+  },
+};
